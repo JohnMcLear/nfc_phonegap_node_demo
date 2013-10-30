03a847b87cf5ad292464ab189c094263baca6438
John McLear <john@mclear.co.uk>
2013-10-30 17:35:01 +0000

    idiot

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.set('views', __dirname + '/');
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res) { // use ejs to render
-  res.render("index.html");
+  console.log("yo");
+  res.render("www/index.html");
 });
 
 
@@ -16,7 +17,8 @@ app.get('/access', function(req, res){
   console.log("NFC ndef trying to get down into funky town", req.query.payload);
   // just to show for now..
   if(req.query.payload == "http://twitter.com/johnmclear"){
-    res.send("<script>alert('you win');</script>"); // obviously you would NEVER do this..
+    console.log("mheh");
+    res.send("Hey"); // obviously you would NEVER do this..
   }
 });
 
